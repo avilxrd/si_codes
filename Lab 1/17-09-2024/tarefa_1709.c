8bfc0e9992db999c681d1db61bd4105a508fb0bf
Miguel Avila de Oliveira <miguelaviladeoliveira@gmail.com>
2024-09-22 13:05:41 -0300

    aulas 19-09-2024, lab 1

---
+++ Lab 1/17-09-2024/tarefa_1709.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	int i, nome[6] = {77, 105, 103, 117, 101, 108};
+
+	for(i=0; i<6; i++){
+		putchar(nome[i]);
+	}
+}
---
+++ Lab 1/19-09-2024/tarefa_1909.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+
+//02. Ignore o enter
+int le_valor3(){
+  int val, enter;
+  int cent, dez, uni;
+
+  cent = getchar() - '0';
+  dez = getchar() - '0';
+  uni = getchar() - '0';
+  enter = getchar();
+
+  val = cent*100 + dez*10 + uni;
+  return val;
+}
+
+
+//04.Quebrar linha ao final
+void imp_valor(int val){
+  int cent, dez, uni;
+
+  uni = val%10;
+  dez = val/10%10;
+  cent = val/100%10;
+
+  putchar(cent + '0');
+  putchar(dez + '0');
+  putchar(uni + '0');
+  putchar('\n');
+}
+
+//01. Media de 3 numeros de 3 digitos
+int media_3(){
+  int num1, num2, num3, media;
+  
+  num1 = le_valor3();
+  num2 = le_valor3();
+  num3 = le_valor3();
+
+  media = (num1 + num2 + num3) / 3;
+  
+  printf("Media --> ");
+  imp_valor(media);
+  putchar('\n');
+
+  return 0;
+}
+
+//03. Leia 1 numero -> Imprima dobro e quadrado
+int dobro_quadrado(){
+  int num, dobro, quadrado;
+  num = le_valor3();
+
+  dobro = num*2;
+  quadrado = num*num;
+
+  imp_valor(dobro);
+  imp_valor(quadrado);
+}
+
+//05. 
+int binario(){
+  int resto;
+  int decimal, metade; // 8 bits -> 0 - 127
+
+  decimal = le_valor3();
+  imp_valor(decimal);
+
+  
+
+}
+
+
+int main(){
+  binario();
+}
