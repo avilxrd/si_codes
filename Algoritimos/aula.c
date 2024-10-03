58b2571a8d6d0412421969b20b6bedaa6f5ebe4d
Miguel Avila de Oliveira <miguelaviladeoliveira@gmail.com>
2024-10-03 12:20:25 -0300

    adicionei aula online

---
+++ Algoritimos/aula.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+//Exercicio 3
+void operacoes(){
+    float num1, num2;
+
+    printf("Digite um numero: ");
+    scanf("%f", &num1);
+    printf("Digite outro numero: ");
+    scanf("%f", &num2);
+
+    printf("\nSoma: %.2f\n", num1+num2);
+    printf("Subtracao: %.2f\n", num1-num2);
+    printf("Multiplicacao: %.2f\n", num1*num2);
+    printf("Divisao: %.2f\n\n", num1/num2);
+}
+
+// //Exercicio 4
+// void dobro(){
+//     float num;
+//     printf("Digite um numero: ");
+//     scanf("%f", &num);
+//     printf("Dobro: %.2f\n", num*2);
+// }
+
+int main(){
+    operacoes();
+}
