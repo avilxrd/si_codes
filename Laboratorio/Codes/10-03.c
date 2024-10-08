3c17a2efe8ef174a463ee4ebe0ee0238ddf56531
Miguel Avila de Oliveira <miguelaviladeoliveira@gmail.com>
2024-10-08 11:02:32 -0300

    exercicios da aula 03/10 - estruturas de decisao e bool

--- Laboratorio/Codes/10-03.c
+++ Laboratorio/Codes/10-03.c
@@ -70,6 +70,190 @@ int gasolina_alcool(){
 
 }
 
+//=============== Exercicios 03/10/2024 ===============//
+int exercicio_01(){
+  float num_1, num_2;
+  scanf("%f", &num_1);  
+  scanf("%f", &num_2);  
+
+  if(num_1 > num_2){
+    printf("%f", num_1);
+  }else if(num_2 > num_1){
+    printf("%f", num_2);
+  }else{
+    printf("Os numeros sao iguais");
+  }
+}
+
+int exercicio_02(){
+  float num_1, num_2;
+  scanf("%f", &num_1);  
+  scanf("%f", &num_2);  
+
+  if(num_1 < num_2){
+    printf("%f", num_1);
+  }else if(num_2 < num_1){
+    printf("%f", num_2);
+  }else{
+    printf("Os numeros sao iguais");
+  }
+}
+
+int exercicio_03(){
+  int num;
+  if(num % 2 == 0){
+    printf("O numero %d e par", num);
+  }else{
+    printf("O numero %d e impar", num);
+  }
+}
+
+int exercicio_04(){
+  float num_1, num_2, num_3;
+  scanf("%f", &num_1);
+  scanf("%f", &num_2);
+  scanf("%f", &num_3);
+
+  if(num_1 > num_2 && num_1 > num_3){
+    printf("Maior: %f", num_1);
+  }else if(num_2 > num_1 && num_2 > num_3){
+    printf("Maior: %f", num_2);
+  }else if(num_3 > num_1 && num_3 > num_2){
+    printf("Maior: %f", num_3);
+  }else{
+    printf("Todos os numeros sao iguais");
+  }
+}
+
+int exercicio_05(){
+  float num_1, num_2, num_3;
+  scanf("%f", &num_1);
+  scanf("%f", &num_2);
+  scanf("%f", &num_3);
+
+  if((num_1 > num_2 && num_1 < num_3)||(num_1 < num_2 && num_1 > num_3)){
+    printf("%f", num_1);
+  }else if ((num_2 < num_1 && num_2 > num_3) || (num_2 > num_1 && num_2 < num_3)){
+    printf("%f", num_2);
+  }else if ((num_3 < num_1 && num_3 > num_2) || (num_3 > num_1 && num_3 < num_2)){
+    printf("%f", num_3);
+  }
+}
+
+int exercicio_06(){
+  float nota_1, nota_2;
+  float peso_1, peso_2;
+  scanf("%f", &nota_1);
+  scanf("%f", &nota_2);
+  scanf("%f", &peso_1);
+  scanf("%f", &peso_2);
+
+  float media_pond = (((nota_1 * peso_1)+(nota_2 * peso_2))/(peso_1 + peso_2));
+  if(media_pond >= 7){
+    printf("Aluno aprovado com media %.2f", media_pond);
+  }else{
+    float nota_exame, media_final;
+    scanf("%f", &nota_exame);
+    media_final = (media_pond + nota_exame)/2;
+    if(media_final >= 5){
+      printf("Aluno aprovado com media %.2f", media_final);
+    }else{
+      printf("Aluno reprovado com media %.2f", media_final);
+    }
+  }
+}
+
+int exercicio_07(){
+  float temperatura;
+  scanf("%f", &temperatura);
+  if(temperatura < 18){
+    printf("Casaco.");
+  }else{
+    printf("Camiseta.");
+  }
+}
+
+int exercicio_08(){
+  float temperatura, vento;
+  scanf("%f", &temperatura);
+  scanf("%f", &vento);
+  if(vento > 10 && temperatura < 20){
+    printf("Casaco.");
+  }else if(vento < 10 && temperatura < 18){
+    printf("Casaco.");
+  }else{
+    printf("Camiseta.");
+  }
+}
+
+int exercicio_09(){
+  float temperatura, vento;
+  int chuva = 0;
+
+  scanf("%f", &temperatura);
+  scanf("%f", &vento);
+  scanf("%d", &chuva);
+
+  if(chuva == 1 || temperatura > 35){
+    printf("Levar guarda chuva.");
+  }
+  
+  if(vento > 10 && temperatura < 20){
+    printf("Casaco.");
+  }else if(vento < 10 && temperatura < 18){
+    printf("Casaco.");
+  }else{
+    printf("Camiseta.");
+  }
+}
+
+float aliquota_ir(float base){
+  float aliquota;
+  if(base <= 2259.20){
+    aliquota = 0;
+  }else if(base >= 2259.21 && base <= 2826.65){
+    aliquota = 0.075;
+  }else if(base >= 2826.66 && base <= 3751.05){
+    aliquota = 0.15;
+  }else if(base >= 3751.06 && base <= 4664.68){
+    aliquota = 0.225;
+  }else{
+    aliquota = 0.275;
+  }
+  return aliquota;
+}
+float parcela_ir(float base){
+  float parcela;
+  if(base <= 2259.20){
+    parcela = 0;
+  }else if(base >= 2259.21 && base <= 2826.65){
+    parcela = 169.44;
+  }else if(base >= 2826.66 && base <= 3751.05){
+    parcela = 381.44;
+  }else if(base >= 3751.06 && base <= 4664.68){
+    parcela = 662.77;
+  }else{
+    parcela = 896.0;
+  }
+  return parcela;
+}
+float calculo_ir(){
+  float base_ir;
+  float aliquota, parcela;
+  float ir;
+
+  scanf("%f", &base_ir);
+  aliquota = aliquota_ir(base_ir);
+  parcela = parcela_ir(base_ir);
+
+  ir = (base_ir * aliquota) - parcela;
+
+  printf("Aliquota IR: %.2f\n", aliquota);
+  printf("Parcela IR: %.2f\n", parcela);
+  printf("Valor do IR: %.2f\n", ir);
+  return ir;
+}
+
 int main(){
-    gasolina_alcool();
+  calculo_ir();
 }
--- Laboratorio/aulas.md
+++ Laboratorio/aulas.md
@@ -43,3 +43,31 @@ int main(){
 2. Faça um programa que lê o preço de um litro de combustível, o consumo do veículo em km/l e a distância a percorrer, e calcula e imprime o custo em reais do combustível para percorrer essa distância. Use variáveis `float`; imprima o valor em reais com 2 casas, para representar os centavos. Informe ao usuário o que é o valor que se espera que ele digite a cada vez (para não ficar só a tela preta e o usuário tendo que conhecer a ordem em que os dados devem ser digitados).
 3. Faça um programa semelhante ao anterior, para um carro elétrico, em que pergunta o valor do kWh, a capacidade da bateria em kWh, a autonomia do veículo em km com bateria cheia e a distância a percorrer em km.
 4. Faça um programa que lê a largura, o comprimento e a profundidade de uma piscina em metros, e imprime sua capacidade em litros. A piscina tem o formato de um paralelepípedo._
+
+# 03/10
+### Exercícios
+Faça um programa que:
+1. Lê dois números, imprime qual o maior.
+1. Lê dois números, imprime "sao iguais" ou imprime qual o menor.
+1. Lê um número, diz se é par ou ímpar (use o resto da divisão por 2 para decidir).
+1. Lê 3 números, imprime qual o maior.
+1. Lê 3 números, imprime qual o do meio.
+5. Lê 2 números entre 0 e 10, que representam as notas de um aluno, e mais dois números, que representam os pesos de cada nota. Se a média ponderada das notas for pelo menos 7, informa que o aluno está aprovado. Se não for, lê mais um número, que é a nota do exame. Se a média entre o exame e a média anterior for pelo menos 5, informa que o aluno está aprovado. Senão, informa que o aluno reprovou.
+6. Lê a temperatura prevista, e recomenda o usuário sobre a roupa a usar. Se a
+   temperatura for inferior a 18 graus, deve sugerir casaco, senão, camiseta.
+7. Altere o programa anterior para também ler a velocidade prevista do vento.
+   Com vento superior a 10 km/h, a temperatura para casaco é 20°.
+8. Altere o programa anterior para também ler a previsão de chuva ('S' ou 'N').
+   O programa deve também responder se o usuário deve levar guarda-chuva se
+   houver previsão de chuva. Também deve levar guarda-chuva se a temperatura for
+   superior a 35 graus.
+
+### Exercícios
+
+1. Faça um programa que pede ao usuário o valor da base de cálculo do imposto
+   de renda e imprime o valor da alíquota e da parcela a deduzir correspondentes.
+2. Altere o programa anterior, de forma que tenha uma função que recebe a base
+   de cálculo e retorna a alíquota, e outra que recebe a base de cálculo e
+   retorna a parcela a deduzir. A impressão deve ser feita pela função
+   principal.
+3. Altere o programa anterior para calcular e imprimir o imposto de renda devido.
