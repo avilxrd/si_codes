c6d4cce0424649dbf510f59deb67ea09ed969a08
Miguel Avila de Oliveira <85706671+avilxrd@users.noreply.github.com>
2024-10-17 16:14:56 -0300

    Update trabalho_01.c

--- Laboratorio/Trabalhos/trabalho_01.c
+++ Laboratorio/Trabalhos/trabalho_01.c
@@ -30,35 +30,32 @@ bool verifica_inteiro(int tamanho_desenho){
     }
 }
 
-void imprime_desenho(char opcao_desenho){
-    switch (opcao_desenho){
-        case 'a': desenho_a();
-        break;
-        case 'b': desenho_b();
-        break;
-        case 'c': desenho_c();
-        break;
-        case 'd': desenho_d();
-        break;
-        case 'e': desenho_e();
-        break;
-        case 'f': desenho_f();
-        break;
-        case 'g': desenho_g();
-        break;
-        case 'h': desenho_h();
-        break;
+//desenhos
+void linha(int n, char c){    
+    for(int i=0; i<n; i++){
+        printf("%c", c);
+    }
+    printf("\n");
+} 
+
+void figura_a(char char_desenhado, int tamanho_desenho){
+    int i;
+    for (i=1; i<tamanho_desenho+1; i++){
+        linha(i, char_desenhado);
     }
 }
 
-//desenhos
-void figura_a(){
-    
+void imprime_desenho(char opcao_desenho, char char_desenhado, int tamanho_desenho){
+    switch (opcao_desenho){
+        case 'a': figura_a(char_desenhado, tamanho_desenho);
+        break;
+        default: printf("kkkk");
+    }
 }
 
+
 void desenha_figura(){
-    printf("Olá usuário, qual desenho você gostaria de fazer?\n");
-    printf("a | b | c | d | e | f | g | h \n");
+    printf("Olá usuário, qual desenho você gostaria de fazer? [a-h] \n");
     //declaração de variáveis
     int tamanho_desenho;
     char opcao_figura, char_desenhado;
@@ -66,21 +63,27 @@ void desenha_figura(){
     do{
         //pede uma opção até que ela seja [a - h] ou z
         do{
-            scanf("%c", &opcao_figura);
-        }while(verifica_opcao()==false);
+            printf("Digite opcao_figura: ");
+            scanf(" %c", &opcao_figura);
+        }while(verifica_opcao(opcao_figura)==false);
+
         //se opcao == z -> para o programa
         if (opcao_figura == 'z'){
             break;
         }
+        
         //pede um char até que ele seja [! - ~] na tabela ascii
         do{
-            scanf("%c", &char_desenhado);
-        }while(verifica_char()==false);
+            printf("Digite char_desenhado: ");
+            scanf(" %c", &char_desenhado);
+        }while(verifica_char(char_desenhado)==false);
         //pede um int até ele ser maior que 0
         do{
+            printf("Digite tamanho_desenho:  ");
             scanf("%d", &tamanho_desenho);
-        }while(verifica_inteiro()==false);
+        }while(verifica_inteiro(tamanho_desenho)==false);
 
+        imprime_desenho(opcao_figura, char_desenhado, tamanho_desenho);
     }while(true);
     printf("Fim do programa...\n");
 }
