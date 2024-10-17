396512c32b3fb6ee501aca740d216b0008dc8095
Miguel Avila <miguelaviladeoliveira@gmail.com>
2024-10-17 12:01:46 -0300

    comecei trabs benhas

---
+++ Laboratorio/Trabalhos/trabalho_01.c
@@ -0,0 +1,9 @@
+//t1 l124b Miguel Avila de Oliveira
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+//verifica se a entrada digitada é aceita ou não
+bool verifica_entrada(char entrada){
+
+}
