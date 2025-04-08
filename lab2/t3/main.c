b7f605e08b0545e9d115a33e4b0606c9daa80700
Miguel A. <85706671+avilxrd@users.noreply.github.com>
2025-04-08 12:14:36 -0300

    Update main.c

--- lab2/t3/main.c
+++ lab2/t3/main.c
@@ -1,14 +1,20 @@
 #include "listas.h"
 
-Node* soma_listas(Node* lista1, Node* lista2)
-{
-    Node* resultado = (Node*)malloc(9*sizeof(Node));
-    Node* a=lista1; 
-    Node* b=lista2;
-    for (a, b; a->prox!=NULL && b->prox!=NULL; a=a->prox, b=b->prox)
-    {
-        
+Node* soma_listas(Node* lista1, Node* lista2) {
+    Node* resultado = NULL;
+    Node* a = lista1;
+    Node* b = lista2;
+    int cont=0;
+    int arr[100];
+    while (a != NULL && b != NULL) {
+        arr[cont] = 
+        // resultado = insere_lista(resultado, a->valor + b->valor);
+        cont++;
+        a = a->prox;
+        b = b->prox;
     }
+
+    return resultado;
 }
 
 int main()
@@ -16,10 +22,15 @@ int main()
     Node* num1 = NULL;
     Node* num2 = NULL;
     srand(time(NULL));
-    for (int i=0; i<5; i++) num1 = insere_lista(num1, rand()%9);
-    for (int i=0; i<5; i++) num2 = insere_lista(num2, rand()%9);
+    for (int i=0; i<5; i++) num1 = insere_lista(num1, rand()%4);
+    for (int i=0; i<5; i++) num2 = insere_lista(num2, rand()%4);
     mostra_lista(num1);
     mostra_lista(num2);
 
+    Node* resultado = soma_listas(num1, num2);
+    mostra_lista(resultado);
+
     return 0;
 }
+
+
