c927056a9433c95f5d77a02d173f2c5b21120c0e
Miguel A. <85706671+avilxrd@users.noreply.github.com>
2025-04-10 11:47:36 -0300

    Update main.c

--- lab2/t3/main.c
+++ lab2/t3/main.c
@@ -4,14 +4,38 @@ Node* soma_listas(Node* lista1, Node* lista2) {
     Node* resultado = NULL;
     Node* a = lista1;
     Node* b = lista2;
-    int cont=0;
-    int arr[100];
-    while (a != NULL && b != NULL) {
-        arr[cont] = 
-        // resultado = insere_lista(resultado, a->valor + b->valor);
-        cont++;
-        a = a->prox;
-        b = b->prox;
+
+    a = final_lista(a);
+    b = final_lista(b);
+
+    int carry=0;
+    while (a != NULL && b != NULL)
+    {
+        int res = a->valor + b->valor;
+        if (res > 10)
+        {
+            printf("\ncarry: %d\tres: %d", carry, res);
+        }
+
+        resultado = insere_lista(resultado, res);
+        a = a->ant;
+        b = b->ant;
+    }
+    if (a != NULL)
+    {
+        while (a != NULL)
+        {
+            resultado = insere_lista(resultado, a->valor);
+            a = a->ant;
+        }
+    }
+    else if (b != NULL)
+    {
+        while (b != NULL)
+        {
+            resultado = insere_lista(resultado, b->valor);
+            b = b->ant;
+        }
     }
 
     return resultado;
@@ -22,8 +46,8 @@ int main()
     Node* num1 = NULL;
     Node* num2 = NULL;
     srand(time(NULL));
-    for (int i=0; i<5; i++) num1 = insere_lista(num1, rand()%4);
-    for (int i=0; i<5; i++) num2 = insere_lista(num2, rand()%4);
+    for (int i=0; i<3; i++) num1 = insere_lista(num1, rand()%9);
+    for (int i=0; i<3; i++) num2 = insere_lista(num2, rand()%9);
     mostra_lista(num1);
     mostra_lista(num2);
 
