6e326285196d8b1a63211112d2d8f028680435fc
Miguel Avila de Oliveira <miguelaviladeoliveira@gmail.com>
2025-04-13 22:45:16 -0300

    subtracao que nao de negativo

--- lab2/t3/main.c
+++ lab2/t3/main.c
@@ -49,6 +49,60 @@ Node* soma_listas(Node* lista1, Node* lista2) {
     return resultado;
 }
 
+Node* subtrai_listas(Node* lista1, Node* lista2)
+{
+    Node* resultado = NULL;
+    Node* a = lista1;
+    Node* b = lista2;
+
+    //se o resultado for negativo quebra
+    if (a->valor <= b->valor)
+    {
+        printf("\nEncerrando pois nao trato negativos ainda...\n");
+        return 0;
+    }
+
+    a = final_lista(a);
+    b = final_lista(b);
+    
+
+    int carry=0;
+    while (a != NULL && b != NULL)
+    {
+
+        if (a->valor >= b->valor)
+        {
+            resultado = insere_lista(resultado, a->valor - b->valor);
+        }
+        else
+        {
+            a->ant->valor -= 1;
+            a->valor += 10;
+            resultado = insere_lista(resultado, a->valor - b->valor);  
+        }
+        a = a->ant;
+        b = b->ant;
+    }
+
+    if (a != NULL)
+    {
+        while (a != NULL)
+        {
+            resultado = insere_lista(resultado, a->valor);
+            a = a->ant;
+        }
+    }
+    else if (b != NULL)
+    {
+        while (b != NULL)
+        {
+            resultado = insere_lista(resultado, b->valor);
+            b = b->ant;
+        }
+    }
+    return resultado;
+}
+
 Node* multiplica_lista(Node* lista1, int valor)
 {
     Node* resultado = lista1;
@@ -68,8 +122,8 @@ int main()
     Node* num2 = NULL;
 
     //preenchendo as listas
-    for (int i=0; i<100; i++) num1 = insere_lista(num1, rand()%9);
-    for (int i=0; i<100; i++) num2 = insere_lista(num2, rand()%9);
+    for (int i=0; i<3; i++) num1 = insere_lista(num1, rand()%9);
+    for (int i=0; i<3; i++) num2 = insere_lista(num2, rand()%9);
 
     //mostrando os numeros
     printf("\nNumero 1: \n");
@@ -78,17 +132,23 @@ int main()
     mostra_lista(num2);
 
     //somando um numero com o outro
+    // printf("\nSoma das listas: \n");
+    // Node* resultado = soma_listas(num1, num2);
+    // mostra_lista(resultado);
+
+    //subtraindo um numero com o outro
     printf("\nSoma das listas: \n");
-    Node* resultado = soma_listas(num1, num2);
+    Node* resultado = subtrai_listas(num1, num2);
     mostra_lista(resultado);
 
+
     //multiplicando por 2 (para testar, mas pode ser qualquer int (a principio)).
-    printf("\nMultiplicando por 2: \n");
-    mostra_lista(num1);
-    printf("x 2\n");
-    printf("___\n");
-    resultado = multiplica_lista(num1, 2);
-    mostra_lista(resultado);
+    // printf("\nMultiplicando por 2: \n");
+    // mostra_lista(num1);
+    // printf("x 2\n");
+    // printf("___\n");
+    // resultado = multiplica_lista(num1, 2);
+    // mostra_lista(resultado);
 
 
     return 0;
