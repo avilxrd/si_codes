57a13f94e179f50640a8d530bcc85d76b56f331b
Miguel A. <85706671+avilxrd@users.noreply.github.com>
2025-04-15 12:04:06 -0300

    Create pilhas.c

---
+++ lab2/t4/pilhas.c
@@ -0,0 +1,59 @@
+#include "pilhas.h"
+
+Stack* cria_pilha(int max_elementos)
+{
+    Stack* pilha = (Stack*)malloc(sizeof(Stack));
+    pilha->max_elementos = max_elementos;
+    pilha->topo          = -1;
+    pilha->item          = (int*)malloc(max_elementos * sizeof(int)); 
+    return pilha;
+}
+
+void mostra_pilha(Stack* pilha)
+{
+    if (vazia(pilha)) return;
+    int quant_elementos = pilha->topo + 1;
+    printf("\nImprimindo uma pilha:\n");
+    for (int i=0; i<quant_elementos; i++) printf("\npilha[%d]:\t%d", i, pilha->item[i]);
+}
+
+
+int vazia(Stack* pilha)
+{
+    // printf("\nA pilha está vazia...\n");
+    return (pilha->topo == -1);
+}
+
+int cheia(Stack* pilha)
+{
+    // printf("\nA pilha está cheia...\n");
+    return (pilha->topo == pilha->max_elementos - 1);
+}
+
+void push(Stack* pilha, int elemento)
+{
+    if (cheia(pilha)) return;
+    pilha->topo++;                         //incrementa o índice do elemento do topo
+    pilha->item[pilha->topo] = elemento;   //adiciona o elemento ao array dos itens
+}
+
+int pop(Stack* pilha)
+{
+    if (vazia(pilha)) return 69;
+    int x = pilha->item[pilha->topo];
+    pilha->topo--;                         //decrementa o índice do elemento do topo
+    return x;
+}
+
+int topo(Stack* pilha)
+{
+    if (vazia(pilha)) return 69;
+    return pilha->item[pilha->topo];
+}
+
+void destroy(Stack* pilha)
+{
+    free(pilha->item);                     //libera a região de memória onde estão os elementos
+    free(pilha);                           //libera a região de memoria da pilha
+    pilha = NULL;                          //arruma o ponteiro da pilha para NULL
+}
