e58c3f632230c2515d560a3325dfc418d1f8fabd
Miguel A. <85706671+avilxrd@users.noreply.github.com>
2025-06-05 11:38:52 -0300

    Update pilhas.c

--- lab2/t9/pilhas.c
+++ lab2/t9/pilhas.c
@@ -2,10 +2,9 @@
 
 Pilha* cria_pilha(int max)
 {
-    Pilha *pilha;
-    pilha->max_elementos = max;
+    Pilha *pilha = (Pilha*) malloc(sizeof(Pilha));
     pilha->topo = -1;
-    pilha->elementos = (int*) malloc (max * sizeof(int));
+    pilha->elementos = (char*) malloc (max * sizeof(char));
     return pilha;
 }
 
@@ -19,15 +18,15 @@ int pilha_vazia(Pilha *pilha)
     return 0;
 }
 
-int pilha_cheia(Pilha *pilha)
-{
-    if (pilha->topo == pilha->max_elementos -1)
-    {
-        printf("\nPilha cheia!\n");
-        return 1;
-    }
-    return 0;
-}
+// int pilha_cheia(Pilha *pilha)
+// {
+//     if (pilha->topo == pilha->max_elementos -1)
+//     {
+//         printf("\nPilha cheia!\n");
+//         return 1;
+//     }
+//     return 0;
+// }
 
 int pop(Pilha *pilha)
 {
@@ -36,19 +35,25 @@ int pop(Pilha *pilha)
         printf("\nNão foi feito o pop()\n");
         return 0;
     }
-    int remov = pilha->elementos[pilha->topo]; 
-    pilha->elementos[pilha->topo] = 0;
+    pilha->elementos[pilha->topo] = '0';
     pilha->topo--;
-    return remov;
 }
 
-Pilha* push(Pilha *pilha)
+Pilha* push(Pilha *pilha, const char valor)
 {
-    if (pilha_cheia(pilha) == 1)
-    {
-        printf("\nNão foi feito o push()\n");
-        return pilha;
-    }
+    // if (pilha_cheia(pilha) == 1)
+    // {
+    //     printf("\nNão foi feito o push()\n");
+    //     return pilha;
+    // }
+    pilha->topo++;
+    pilha->elementos[pilha->topo] = valor;
 
-    
+    return pilha;
+}
+
+void imprime_pilha(Pilha *pilha)
+{
+    if (pilha_vazia(pilha) == 1) return;
+    for (int i=pilha->topo; i>=0; i--) printf("\n[%d] %c", i, pilha->elementos[i]);
 }
